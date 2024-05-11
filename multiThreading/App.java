2bed52b3a048fa14a173b17e15c5f947b55e3d20
cjuanik <anik.patra2014@gmail.com>
2024-05-11 17:28:15 +0530

    Add MultiThreading Basic Commit

---
+++ multiThreading/App.java
@@ -0,0 +1,11 @@
+package multiThreading;
+
+public class App {
+
+    public static void main(String[] args) {
+
+        MTSample mtSample = new MTSample();
+        mtSample.start();
+    }
+
+}
---
+++ multiThreading/MTSample.java
@@ -0,0 +1,11 @@
+package multiThreading;
+import java.lang.Thread;
+
+public class MTSample extends Thread{
+    public void run(){
+
+        System.out.println("hello multithread");
+    }
+
+
+}
